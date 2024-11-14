506db99636dcc59e09751d838be83f1269deb445
juliensl31 <47530405+juliensl31@users.noreply.github.com>
2024-11-14 13:24:25 +0100

    maj next.config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  reactStrictMode: true,
   // Désactive le spinner Vercel
   devIndicators: {
-    buildActivity: false,
-    output: 'export',  // Génère des fichiers statiques
-    basePath: '/cineverse',  // Nom de votre repo GitHub
+    buildActivity: false
+  },
+  output: 'export',  // Génère des fichiers statiques
+  assetPrefix: isProd ? '/cineverse/' : '',
+  basePath: isProd ? '/cineverse' : '',
   images: {
     unoptimized: true,  // Nécessaire pour l'export statique
   },
-  }
 }
+
 
 module.exports = nextConfig
