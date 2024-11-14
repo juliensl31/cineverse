aff544459a34d5c110bca8a73cc071ecce2d71c6
juliensl31 <47530405+juliensl31@users.noreply.github.com>
2024-11-14 15:49:05 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   // Désactive le spinner Vercel
   devIndicators: {
     buildActivity: false
   },
   output: 'export',
-  assetPrefix: isProd ? '/cineverse/' : '',
-  basePath: isProd ? '/cineverse' : '',
+  basePath:'/',
   images: {
     unoptimized: true,
   },
